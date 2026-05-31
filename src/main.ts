12996cbbd208030c749b3fbea17f7f4cc30bb253
GalangAgnarr <agnarwidajanto@gmail.com>
2026-05-31 17:48:19 +0700

    fix: use PORT env variable

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(3000);
-  console.log('Server running at http://localhost:3000');
-  console.log('Swagger docs at http://localhost:3000/api-docs');
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Swagger docs at http://localhost:${port}/api-docs`);
 }
 bootstrap();
